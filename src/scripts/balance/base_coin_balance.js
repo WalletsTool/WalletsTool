c1913a7b884ac8f85b1f90d3201ec319773d2847
renmk <renmk@neuqsoft.com>
2023-11-19 09:01:22 +0800

    fixed: 增加SOL链余额查询功能

--- src/scripts/balance/base_coin_balance.js
+++ src/scripts/balance/base_coin_balance.js
@@ -1,5 +1,7 @@
 import {utils} from "@/scripts/common/provider.js";
 import {ethers} from "ethers";
+import * as web3 from "@solana/web3.js";
+import bs58 from "bs58";
 
 const base_coin_balance = {
 
@@ -8,20 +10,34 @@ const base_coin_balance = {
         return new Promise(async (resolve, reject) => {
             // 随机获取rpc服务
             const provider = utils.get_provider(chain)
-            console.log('当前RPC地址：',provider.connection.url)
             try {
-                // 通过私钥创建钱包
-                let wallet = new ethers.Wallet(item.private_key, provider);
-                wallet.getBalance().then((balance) => {
-                    item.plat_balance = parseFloat(ethers.utils.formatEther(balance)).toFixed(6).toString()
-                    resolve()
-                }).catch((err) => {
-                    item.plat_balance = ''
-                    item.error_msg = '查询平台余额失败！'
-                    console.log('查询平台余额失败！', err)
-                    resolve()
-                })
-            }catch (err){
+                if (chain === 'sol') {
+                    console.log('当前RPC地址：', provider._rpcEndpoint)
+                    const keypair = web3.Keypair.fromSecretKey(bs58.decode(item.private_key))
+                    provider.getBalance(keypair.publicKey).then((balance_lam) => {
+                        item.plat_balance = balance_lam / web3.LAMPORTS_PER_SOL;
+                        resolve()
+                    }).catch((err) => {
+                        item.plat_balance = ''
+                        item.error_msg = '查询平台余额失败！'
+                        console.log('查询平台余额失败！', err)
+                        resolve()
+                    })
+                } else {
+                    console.log('当前RPC地址：', provider.connection.url)
+                    // 通过私钥创建钱包
+                    let wallet = new ethers.Wallet(item.private_key, provider);
+                    wallet.getBalance().then((balance) => {
+                        item.plat_balance = parseFloat(ethers.utils.formatEther(balance)).toFixed(6).toString()
+                        resolve()
+                    }).catch((err) => {
+                        item.plat_balance = ''
+                        item.error_msg = '查询平台余额失败！'
+                        console.log('查询平台余额失败！', err)
+                        resolve()
+                    })
+                }
+            } catch (err) {
                 item.plat_balance = ''
                 item.error_msg = '创建钱包失败，请检查私钥是否正确！'
                 console.log('创建钱包失败！', err)
@@ -34,20 +50,35 @@ const base_coin_balance = {
         return new Promise(async (resolve, reject) => {
             // 随机获取rpc服务
             const provider = utils.get_provider(chain)
-            console.log('当前RPC地址：',provider.connection.url)
-            const nonce = provider.getTransactionCount(item.address)
-            const balance_wei = provider.getBalance(item.address)
-            Promise.all([balance_wei, nonce]).then(([balance_wei, nonce]) => {
-                item.plat_balance = parseFloat(ethers.utils.formatEther(balance_wei)).toFixed(6).toString()
-                item.nonce = nonce
-                resolve()
-            }).catch((err) => {
-                item.nonce = ''
-                item.plat_balance = ''
-                item.error_msg = '查询平台余额失败！'
-                console.log('查询平台余额失败！', err)
-                resolve()
-            })
+            if (chain === 'sol') {
+                console.log('当前RPC地址：', provider._rpcEndpoint)
+                provider.getBalance(new web3.PublicKey(item.address)).then((balance_lam) => {
+                    item.nonce = ''
+                    item.plat_balance = balance_lam / web3.LAMPORTS_PER_SOL;
+                    resolve()
+                }).catch((err) => {
+                    item.plat_balance = ''
+                    item.nonce = ''
+                    item.error_msg = '查询平台余额失败！'
+                    console.log('查询平台余额失败！', err)
+                    resolve()
+                })
+            } else {
+                console.log('当前RPC地址：', provider.connection.url)
+                const nonce = provider.getTransactionCount(item.address)
+                const balance_wei = provider.getBalance(item.address)
+                Promise.all([balance_wei, nonce]).then(([balance_wei, nonce]) => {
+                    item.plat_balance = parseFloat(ethers.utils.formatEther(balance_wei)).toFixed(6).toString()
+                    item.nonce = nonce
+                    resolve()
+                }).catch((err) => {
+                    item.nonce = ''
+                    item.plat_balance = ''
+                    item.error_msg = '查询平台余额失败！'
+                    console.log('查询平台余额失败！', err)
+                    resolve()
+                })
+            }
         })
     }
 }

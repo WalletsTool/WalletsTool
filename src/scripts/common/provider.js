b4b90aad64c5d788137cf1881cfa61191e258e90
renmengkai <renmengkai@gmail.com>
2024-05-23 10:58:48 +0800

    fixed: 增加Scroll正式网的支持

--- src/scripts/common/provider.js
+++ src/scripts/common/provider.js
@@ -150,16 +150,24 @@ const providers = {
     scroll_eth_provider() {
         // rpc 节点
         const rpc_list = [
+            'https://scroll-mainnet-public.unifra.io',
+            'https://scroll.blockpi.network/v1/rpc/public',
+            'https://scroll.drpc.org',
+            'https://scroll-mainnet.public.blastapi.io',
+            'https://scroll-mainnet.rpc.grove.city/v1/a7a7c8e2',
+            'https://1rpc.io/scroll',
+            'https://rpc.ankr.com/scroll',
+            'https://scroll-mainnet.chainstacklabs.com',
             // 'https://scroll-alphanet.blastapi.io/d6df6b9f-6b9a-470e-b529-708c36a65c32', // blastapi_rpc
-            'https://scroll-alphanet.public.blastapi.io',
-            'https://alpha-rpc.scroll.io/l2',
-            'https://scroll-testnet.blockpi.network/v1/rpc/public',
-            'https://scroll-alpha-public.unifra.io',
+            // 'https://scroll-alphanet.public.blastapi.io',
+            // 'https://alpha-rpc.scroll.io/l2',
+            // 'https://scroll-testnet.blockpi.network/v1/rpc/public',
+            // 'https://scroll-alpha-public.unifra.io',
             // 'https://scroll-alpha.unifra.io/v1/5c1ce5f1ab9b4fc6b063b8e9afc58f5c', // unifra_rpc
         ]
         const rpc_url = rpc_list[Math.floor(Math.random() * rpc_list.length)]
 
-        return new ethers.providers.JsonRpcProvider(rpc_url, 534353)
+        return new ethers.providers.JsonRpcProvider(rpc_url, 534352)
     },
     binance_provider() {
         // rpc 节点
